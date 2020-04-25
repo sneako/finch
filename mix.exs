20197132f3d9ac6eda99335a41a2de90750d86da
Nico Piderman <nico.piderman@gmail.com>
2020-04-25 14:16:39 +0200

    update mix.exs with hex metadata

--- mix.exs
+++ mix.exs
@@ -1,12 +1,21 @@
 defmodule Finch.MixProject do
   use Mix.Project
 
+  @name "Finch"
+  @version "0.1.0"
+  @repo_url "https://github.com/keathley/finch"
+
   def project do
     [
       app: :finch,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.7",
+      description: "An HTTP client focused on performance.",
+      package: package(),
+      docs: docs(),
       start_permanent: Mix.env() == :prod,
+      name: @name,
+      source_url: @repo_url,
       deps: deps()
     ]
   end
@@ -27,6 +36,21 @@ defmodule Finch.MixProject do
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
       {:credo, "~> 1.3", only: [:dev, :test]},
       {:bypass, "~> 1.0", only: :test}
+    ]
+  end
+
+  def package do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => @repo_url}
+    ]
+  end
+
+  def docs do
+    [
+      source_ref: "v#{@version}",
+      source_url: @repo_url,
+      main: @name
     ]
   end
 end
