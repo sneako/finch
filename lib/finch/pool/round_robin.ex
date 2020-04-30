72a8e2de30672f1903a30768711295869e2a76b8
Nico Piderman <nico.piderman@gmail.com>
2020-04-30 12:34:23 +0200

    do not name the ets table and pass around the table reference instead

--- lib/finch/pool/round_robin.ex
+++ lib/finch/pool/round_robin.ex
@@ -5,20 +5,20 @@ defmodule Finch.Pool.RoundRobin do
   @behaviour Finch.Pool.Strategy
 
   @impl true
-  def init_pool_group(shp, %{ets_name: ets}, _pool_config) do
+  def init_pool_group(shp, %{ets_table: ets}, _pool_config) do
     :ets.insert(ets, {shp, -1})
     :ok
   end
 
   @impl true
-  def registry_value(shp, %{ets_name: ets}, %{count: count}) do
-    %{strategy: __MODULE__, shp: shp, count: count, ets_name: ets}
+  def registry_value(shp, %{ets_table: ets}, %{count: count}) do
+    %{strategy: __MODULE__, shp: shp, count: count, ets_table: ets}
   end
 
   @impl true
   def choose_pool([{_, registry_value} | _] = pids) do
-    %{ets_name: name, shp: shp, count: count} = registry_value
-    index = :ets.update_counter(name, shp, {2, 1, count, 1})
+    %{ets_table: ets, shp: shp, count: count} = registry_value
+    index = :ets.update_counter(ets, shp, {2, 1, count, 1})
     {pid, _} = Enum.at(pids, rem(index, count))
 
     pid
