191441731542f66c068137f9c1a429e49d3bd28a
xinz <joy2everyone.z@gmail.com>
2020-07-02 16:55:40 +0800

    Fix finch_test - protocol_not_negotiated

--- test/finch_test.exs
+++ test/finch_test.exs
@@ -276,11 +276,13 @@ defmodule FinchTest do
       assert {:ok, _} = Finch.build(:get, endpoint(bypass)) |> Finch.request(H1Finch)
 
       stop_supervised(Finch)
+    end
 
+    test "SSL - fail to select HTTP2", _bypass do
       start_supervised!({Finch, name: H2Finch, pools: %{default: [protocol: :http2]}})
 
       capture_log(fn ->
-        assert {:error, _} = Finch.build(:get, endpoint(bypass)) |> Finch.request(H2Finch)
+        assert {:error, _} = Finch.build(:get, "https://httpstat.us") |> Finch.request(H2Finch)
       end) =~ "failed to negotiate protocol"
     end
 
