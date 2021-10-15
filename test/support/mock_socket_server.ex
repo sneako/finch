523566377f291962a99fa703ac3d562875c25b4f
Calvin Gerling <calvin@gerling.sh>
2021-10-15 08:43:00 -0300

    add ssl capability to mock socket server

--- test/support/mock_socket_server.ex
+++ test/support/mock_socket_server.ex
@@ -1,18 +1,35 @@
 defmodule Finch.MockSocketServer do
   @moduledoc false
 
+  @fixtures_dir Path.expand("../fixtures", __DIR__)
+
+  @socket_opts [
+    active: false,
+    mode: :binary,
+    packet: :raw
+  ]
+
+  @ssl_opts [
+    reuseaddr: true,
+    nodelay: true,
+    certfile: Path.join([@fixtures_dir, "selfsigned.pem"]),
+    keyfile: Path.join([@fixtures_dir, "selfsigned_key.pem"])
+  ]
+
   @http_response "HTTP/1.1 200 OK\r\n\r\n"
 
-  def start do
+  def start(opts \\ []) do
+    ssl? = Keyword.get(opts, :ssl?, false)
+
     socket_address = build_socket_address()
     delete_existing_sockets(socket_address)
 
-    {:ok, socket} = listen(socket_address)
+    {:ok, socket} = listen(socket_address, ssl?)
 
     spawn_link(fn ->
-      {:ok, client} = accept(socket)
+      {:ok, client} = accept(socket, ssl?)
 
-      serve(client)
+      serve(client, ssl?)
     end)
 
     {:ok, socket_address}
@@ -29,24 +46,54 @@ defmodule Finch.MockSocketServer do
     File.rm(socket_path)
   end
 
-  defp listen(socket_address) do
-    opts = [active: false, mode: :binary, packet: :raw, ifaddr: socket_address]
+  defp listen(socket_address, false = _ssl?) do
+    opts = [{:ifaddr, socket_address} | @socket_opts]
 
     :gen_tcp.listen(0, opts)
   end
 
-  defp accept(socket) do
+  defp listen(socket_address, true = _ssl?) do
+    base_opts = @socket_opts ++ @ssl_opts
+    opts = [{:ifaddr, socket_address} | base_opts]
+
+    :ssl.listen(0, opts)
+  end
+
+  defp accept(socket, false = _ssl?) do
     {:ok, _client} = :gen_tcp.accept(socket)
   end
 
-  defp serve(client) do
+  defp accept(socket, true = _ssl?) do
+    {:ok, client} = :ssl.transport_accept(socket)
+
+    if function_exported?(:ssl, :handshake, 1) do
+      {:ok, _} = apply(:ssl, :handshake, [client])
+    else
+      :ok = apply(:ssl, :ssl_accept, [client])
+    end
+
+    {:ok, client}
+  end
+
+  defp serve(client, false = ssl?) do
     case :gen_tcp.recv(client, 0) do
       {:ok, _data} ->
         :gen_tcp.send(client, @http_response)
         :gen_tcp.close(client)
 
       _ ->
-        serve(client)
+        serve(client, ssl?)
+    end
+  end
+
+  defp serve(client, true = ssl?) do
+    case :ssl.recv(client, 0) do
+      {:ok, _data} ->
+        :ssl.send(client, @http_response)
+        :ssl.close(client)
+
+      _ ->
+        serve(client, ssl?)
     end
   end
 end
