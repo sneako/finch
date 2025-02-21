ed376b56bd1c4b7f60c589ed8cd4b939be69d023
Kian-Meng Ang <kianmeng@cpan.org>
2025-02-21 17:48:23 +0800

    CI housekeeping
    List of changes:
    - bump Credo and related deps to resolve compilation errors
    - set minimum Elixir version to 1.13 due to Credo
    - specify versions as strings, not numbers
    - support Elixir 1.17/Erlang OTP 27 in CI matrix

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Finch.MixProject do
     [
       app: :finch,
       version: @version,
-      elixir: "~> 1.11",
+      elixir: "~> 1.13",
       description: "An HTTP client focused on performance.",
       package: package(),
       docs: docs(),
