64ca19bb3b3de23fe9eb94f063c0ffdca940c15a
Aleksandar <isavitaisa@gmail.com>
2025-09-16 17:54:46 +0200

    Add support for querying default pool metrics

--- test/finch/http1/pool_metrics_test.exs
+++ test/finch/http1/pool_metrics_test.exs
@@ -38,6 +38,36 @@ defmodule Finch.HTTP1.PoolMetricsTest do
     assert {:error, :not_found} = Finch.get_pool_status(finch_name, shp)
   end
 
+  test "get default pool status", %{bypass: bypass, finch_name: finch_name} do
+    start_supervised!(
+      {Finch,
+       name: finch_name, pools: %{default: [protocols: [:http1], start_pool_metrics?: true]}}
+    )
+
+    shp = shp_from_bypass(bypass)
+
+    Bypass.expect_once(bypass, "GET", "/", fn conn ->
+      Plug.Conn.send_resp(conn, 200, "OK")
+    end)
+
+    assert {:ok, %Finch.Response{status: 200}} =
+             Finch.build(:get, endpoint(bypass))
+             |> Finch.request(finch_name)
+
+    wait_connection_checkin()
+
+    assert {:ok, %{^shp => [%PoolMetrics{}]}} = Finch.get_pool_status(finch_name, :default)
+  end
+
+  test "get default pool status returns error when no pools", %{finch_name: finch_name} do
+    start_supervised!(
+      {Finch,
+       name: finch_name, pools: %{default: [protocols: [:http1], start_pool_metrics?: true]}}
+    )
+
+    assert {:error, :not_found} = Finch.get_pool_status(finch_name, :default)
+  end
+
   test "get pool status", %{bypass: bypass, finch_name: finch_name} do
     start_supervised!(
       {Finch,
