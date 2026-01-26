3a5b5797489539c2b96ef9a356d96c0411e5ea76
nico piderman <nico.piderman@gmail.com>
2026-01-26 10:34:50 +0100

    choose a random pool from the list of pools returned by registry

--- lib/finch/pool/manager.ex
+++ lib/finch/pool/manager.ex
@@ -56,8 +56,8 @@ defmodule Finch.Pool.Manager do
       [] ->
         :not_found
 
-      [{pid, pool_mod} | _] ->
-        {pid, pool_mod}
+      pools ->
+        Enum.random(pools)
     end
   end
 
