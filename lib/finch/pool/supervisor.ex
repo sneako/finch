f93ba4b2022475c76177184c9cc8b97bd43a4d9d
Nelson Vides <videsnelson@gmail.com>
2026-03-27 16:18:01 +0100

    Return results of the supervisor operations to the caller

--- lib/finch/pool/supervisor.ex
+++ lib/finch/pool/supervisor.ex
@@ -5,6 +5,8 @@ defmodule Finch.Pool.Supervisor do
   alias Finch.Pool
   use Supervisor, restart: :transient
 
+  @type pool_idx() :: pos_integer()
+
   def start_link({name, arg}) do
     Supervisor.start_link(__MODULE__, arg, name: name)
   end
@@ -22,29 +24,37 @@ defmodule Finch.Pool.Supervisor do
     Supervisor.init(specs, auto_shutdown: :all_significant, strategy: :one_for_one)
   end
 
+  @spec set_count(pid(), Pool.t(), Finch.name(), map(), pos_integer(), pos_integer()) ::
+          :ok | {:error, {pool_idx(), term()}}
   def set_count(sup_pid, pool, registry_name, pool_config, old_count, new_count) do
     cond do
       new_count > old_count ->
-        for pool_idx <- (old_count + 1)..new_count do
+        Enum.reduce_while((old_count + 1)..new_count, :ok, fn pool_idx, :ok ->
           spec = build_child_spec(pool, registry_name, pool_config, pool_idx)
-          Supervisor.start_child(sup_pid, spec)
-        end
 
-        :ok
+          case Supervisor.start_child(sup_pid, spec) do
+            {:ok, _pid} -> {:cont, :ok}
+            {:error, {:already_started, _pid}} -> {:cont, :ok}
+            {:error, reason} -> {:halt, {:error, {pool_idx, reason}}}
+          end
+        end)
 
       new_count < old_count ->
-        for pool_idx <- old_count..(new_count + 1)//-1 do
-          Supervisor.terminate_child(sup_pid, pool_idx)
-          Supervisor.delete_child(sup_pid, pool_idx)
-        end
-
-        :ok
+        Enum.reduce_while(old_count..(new_count + 1)//-1, :ok, fn pool_idx, :ok ->
+          with :ok <- Supervisor.terminate_child(sup_pid, pool_idx),
+               :ok <- Supervisor.delete_child(sup_pid, pool_idx) do
+            {:cont, :ok}
+          else
+            {:error, reason} -> {:halt, {:error, {pool_idx, reason}}}
+          end
+        end)
 
       true ->
         :ok
     end
   end
 
+  @spec build_child_spec(Pool.t(), Finch.name(), map(), pool_idx()) :: Supervisor.child_spec()
   defp build_child_spec(pool, registry_name, pool_config, pool_idx) do
     pool_name = Pool.to_name(pool)
     pool_args = {pool, pool_name, registry_name, pool_config, pool_idx}
